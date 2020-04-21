08d6c7a326f2faf9b8ce4f0a868f5cf0191bf0c9
Nashy Jin <nashyjin@protonmail.com>
2020-04-22 01:22:15 +0900

    Update p116_drill_no01.cpp

--- section_04/p116_drill_no01.cpp
+++ section_04/p116_drill_no01.cpp
@@ -1,28 +1,27 @@
-﻿/*whileループで構成されたプログラムを作成する．
-ループを繰り返すたびにint型の値を２つ読み込み，出力する．
-"|"が入力されたらプログラムを終了する．*/
 #include "../../std_lib_facilities.h"
 
 int main()
 {
-	int a, b;
-	char c {};
-	constexpr char end = '|';
+	int val1{ 0 };
+	int val2{ 0 };
+	char empty;
+	constexpr char end_mark = '|';
+	string user_prompt = { "Enter valid 2 int number, " + string{end_mark} +" to exit: --->" };
 
-	cout << "Please enter 2 integer values.if you want to quit,type | to exit.: ";
-	while (cin >> c && c != end) {  //双方の条件がそろった場合ループを行う(trueの場合)
-		cin.putback(c);  //cをストリームに戻し，次に読み込む文字にする(p1146を参照)
-		if (cin >> a >> b) {
-			cout << "Entered numbers is " << a << ' ' << b << '\n';
+	cout << user_prompt << '\n';
+	while (cin >> empty && empty != end_mark) {
+		cin.putback(empty);			// empty をストリームに戻し，次に読み込む文字にする(p1146を参照)
+		if (cin >> val1 >> val2) {			//整数を二つ入力した場合
+			cout << "Entered number is " << val1 << ' ' << val2 << '\n';
 		}
-		else {
-			cin >> c;
-			if (c == end) {
-				break;
-			}
-			cout << "Please dont enter invalid value,try again: \n";
+		else if (cin >> empty && empty==end_mark) {			//終了マークを入力した場合
+			break;
 		}
-		cout << "Please enter 2 integer value.if you want to quit,type | to exit.: ";
+		cout << user_prompt << '\n';
 	}
-	return 0;
 }
+
+/*
+１	whileループで構成されたプログラムを作成する．whileループではループを繰り返すたびに int 型の値を2つ読み込み，それらを出力する．
+	'|'が入力されたらプログラムを終了する．⏎
+*/
