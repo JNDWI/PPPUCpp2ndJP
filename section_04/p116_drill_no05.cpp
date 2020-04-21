7b4476df7f638f969e6b6c54e1addafbda9af7eb
Nashy Jin <nashyjin@protonmail.com>
2020-04-22 01:32:46 +0900

    Update p116_drill_no05.cpp

--- section_04/p116_drill_no05.cpp
+++ section_04/p116_drill_no05.cpp
@@ -1,41 +1,53 @@
-﻿#include "../../std_lib_facilities.h"
+#include "../../std_lib_facilities.h"
+
+double CM_M_CONV_RATIO = 1.0 / 100;			//近似値の範囲を設定
+
+string near_equal(double val1, double val2,double CM_M_CONV_RATIO)			//2つの値の差が0.01未満の場合出力を変える関数
+{
+	if (abs(val1 - val2) < CM_M_CONV_RATIO)			//abs関数(p1156)		//val1とval2の差の絶対値が1.0/100より小さい場合
+		return string{ "the numbers are almost equal\n" };
+	else			//それ以外の場合
+		return string{ "" };
+}
 
 int main()
 {
-	double a, b;
-	char c{};
-	constexpr char end = '|';
+	double val1{ 0 };
+	double val2{ 0 };
+	char empty;
+	constexpr char end_mark = '|';
+	string user_prompt = { "Enter valid 2 floating point numbers, " + string{end_mark} +" to exit: --->" };
 
-	cout << "Please enter 2 floating point values.if you want to quit,type | to exit.: ";
-	while (cin >> c && c != end) {
-		cin.putback(c);
-		if (cin >> a >> b) {
-			double d = abs(a - b);  //abs関数(p1156を参照)
-			cout << "Entered numbers is " << a << ' ' << b << '\n';
-			if (a < b) {
-				cout << "the smaller value is: " << a << '\n';
-				cout << "the larger value is: " << b << "\n";
-				if (d < 1.0 / 100)  //差が0.01未満なら
-					cout << "the number is almost equal\n";
+	cout << user_prompt << '\n';
+	while (cin >> empty && empty != end_mark) {
+		cin.putback(empty);
+		if (cin >> val1 >> val2) {
+			if (val1 < val2) {
+				cout << "the smallest value is: " << val1 << '\n'
+					<< "the largest value is : " << val2 << '\n'
+					<< near_equal(val1, val2, CM_M_CONV_RATIO);			//関数使用
 			}
-			else if (a == b) {
+			else if (val1 == val2) {
 				cout << "the numbers are equal\n";
 			}
 			else {
-				cout << "the smaller value is: " << b << '\n';
-				cout << "the larger value is: " << a << "\n";
-				if (d < 1.0 / 100)  //差が0.01未満なら
-					cout << "the number is almost equal\n";
+				cout << "the smallest value is: " << val2 << '\n'
+					<< "the largest value is : " << val1 << '\n'
+					<< near_equal(val1, val2, CM_M_CONV_RATIO);			//関数使用
 			}
 		}
-		else {
-			cin >> c;
-			if (c == end) {
-				break;
-			}
-			cout << "Please dont enter invalid value,try again: \n";
+		else if (cin >> empty && empty == end_mark) {
+			break;
 		}
-		cout << "Please enter 2 floating point value.if you want to quit,type | to exit.: ";
+		cout << user_prompt << '\n';
 	}
-	return 0;
 }
+
+/*
+１	whileループで構成されたプログラムを作成する．whileループではループを繰り返すたびに int 型の値を2つ読み込み，それらを出力する．
+	'|'が入力されたらプログラムを終了する．⏎
+２	"the smallest value is: "に続いて小さいほうの数字をかき出し，"the larger value is: "に続いて大きいほうの値を書き出すように変更する．⏎
+３	2つの数字が等しい場合に（のみ），"the numbers are equal"行をかき出すように変更する．⏎
+４	int の代わりに doubleを使用するように変更する．⏎
+５	2つの数字の差が1.0/100見なんである場合に，どちらの数字が大きいかをかき出した後，"the numbers is almost equal"と書き出すように変更する．⏎
+*/
