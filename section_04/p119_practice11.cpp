e5267ff4ead2c6c9cce2cbeec17b9be8bc47ff87
Nashy Jin <nashyjin@protonmail.com>
2020-04-27 00:58:53 +0900

    Add files via upload

---
+++ section_04/p119_practice11.cpp
@@ -0,0 +1,16 @@
+﻿#include "../../std_lib_facilities.h"
+
+int main()
+{
+	int val;
+	vector <int>primes = { 2 };
+
+	cout << "1から100までの整数を一つ入力してください． ---> ";
+	cin >> val;
+
+	for (int i = 0; i < primes.size(); ++i) {
+		while (val%primes[i] == 0) {
+			primes.push_back(val);
+		}
+	}
+}
