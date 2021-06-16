545b30cce791988187c066f3f39a5dce33386d65
nanonashy <nanonashy@protonmail.com>
2021-06-16 12:11:36 +0900

    Delete section_04 directory

--- section_04/p103_try_this.cpp
+++
@@ -1,8 +0,0 @@
-﻿//p101_try_this.cpp の内容をfor文で書き換える．
-#include "../../std_lib_facilities.h"
-
-int main()
-{
-	for (char roop = 'a'; roop < 'z' + 1; ++roop)
-		cout << roop << '\t' << int(roop) << '\n';
-}
--- section_04/p118_practice01.cpp
+++
@@ -1,6 +0,0 @@
-﻿#include "../../std_lib_facilities.h"
-
-int main()
-{
-	cout << "本章のTRY THISをすべて実行する\n";
-}
