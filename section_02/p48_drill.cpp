32949886a02f66afe228da5539aaa37c740b42e3
nanonashy <nanonashy@protonmail.com>
2021-06-16 12:05:16 +0900

    Delete section_02 directory

--- section_02/p48_drill.cpp
+++
@@ -1,8 +0,0 @@
-﻿#include "../../std_lib_facilities.h"  //std_lib_facilities.hをインクルード
-
-int main()  //C++プログラムはmain関数から実行を開始する
-{
-	cout << "Hello,World!\n";  //"Hello,World!"を出力
-	keep_window_open();  //１文字が入力されるまで待機
-	return 0;
-}
--- section_02/p51_practice.cpp
+++
@@ -1,8 +0,0 @@
-﻿#include "../../std_lib_facilities.h"  //std_lib_facilities.hをインクルード
-
-int main()
-{
-	cout << "Hello,programming!\nHere we go !\n\n";  //Hello,programming! Here we go !を出力、\nで改行を挟んでいる
-	keep_window_open();  //１文字入力されるまで待機
-	return 0; // return 0;　はなくてもOK
-}
