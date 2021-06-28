35101e8530692b0e7fa8f01ab140640dece641ee
nanonashy <nanonashy@protonmail.com>
2021-06-28 13:48:58 +0900

    Add files via upload

---
+++ ch04/4.exercise.02.cpp
@@ -0,0 +1,20 @@
+﻿#include "std_lib_facilities.h"
+
+int main()
+{
+	vector<double> temps;
+	for (double temp; cin >> temp;)
+		temps.push_back(temp);
+
+	double sum = 0;
+	for (int x : temps)sum += x;
+	cout << "Average temperature: " << sum / temps.size() << '\n';
+
+	sort(temps);
+	if (temps.size() % 2 == 0)
+		cout << "Median temperature: " << (temps[temps.size() / 2 - 1] + temps[temps.size() / 2]) / 2 << '\n';
+	else
+		cout << "Median temperature: " << temps[temps.size() / 2] << '\n';
+
+	return 0;
+}
---
+++ ch04/4.exercise.05.cpp
@@ -0,0 +1,41 @@
+﻿#include "std_lib_facilities.h"
+
+int main()
+{
+	double val1{ 0 };
+	double val2{ 0 };
+	char oper{ ' ' };
+
+	cout << "電卓..." << endl;
+	cout << "2つのdouble値と演算子を1つ入力してください。(+,-,*,/) >> ";
+
+	while (cin >> val1 >> val2 >> oper) {
+		switch (oper)
+		{
+		case '+':
+			cout << "The sum of " << val1 << " and " << val2 << " is "
+				<< val1 + val2 << endl;
+			break;
+		case '-':
+			cout << "The difference of " << val1 << " and " << val2 << " is "
+				<< val1 - val2 << endl;
+			break;
+		case '*':
+			cout << "The product of " << val1 << " and " << val2 << " is "
+				<< val1 * val2 << endl;
+			break;
+		case '/':
+			if (val2 == 0)
+				cout << "ERROR: Division by zero\n";
+			else
+				cout << "The division of " << val1 << " and " << val2 << " is "
+				<< val1 / val2 << endl;
+			break;
+		default:
+			break;
+		}
+		cout << ">> ";
+	}
+
+	return 0;
+}
